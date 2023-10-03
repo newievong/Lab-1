1ead266a377f9543499ec471d1db7ecef7dc7548
Newie Vongmanivanh <vongmanivanh.n9@gmail.com>
2023-10-02 21:53:39 -0400

    Lab 1

---
+++ lab1.py
@@ -0,0 +1,43 @@
+#  exercise 1.1
+statement = input("Enter some text: ")
+print(statement)
+
+# exercise 1.2
+inputnumber = int(input("Enter a number: "))
+x = inputnumber + 1
+print(x)
+
+# exercise 1.3
+floatnum = float(input("Enter a number: "))
+y = floatnum + 0.5
+print(y)
+
+# exercise 1.4
+num1 = float(input("Enter a number: "))
+num2 = float(input("Enter another number: "))
+sumof2 = num1 + num2
+print(f"The sum is {sumof2}")
+
+# exercise 1.5
+number1 = float(input("Enter a number: "))
+number2 = float(input("Enter another number: "))
+productof2 = number1 * number2
+print(f"The product is {productof2}")
+
+# exercise 1.6
+divide1 = float(input("Enter a number: "))
+divide2 = float(input("Enter another number: "))
+resultof2 = divide1 / divide2
+print(f"The result is {resultof2}")
+
+# exercise 1.7
+enter1 = input("Enter a boolean: ")
+print("You entered: " + enter1)
+
+if enter1 == "True":
+    print("The opposite of what you entered is: False")
+else:
+    print("The opposite of what you entered is: True")
+
+
+
